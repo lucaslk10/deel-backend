b9f5f5c171eea283309006c8d3b6ba5af8f9a9d2
Lucas Santana <lucas2.4@live.com>
2024-04-04 19:18:54 -0300

    fix: allow contract fetch just for auth user

--- src/controllers/contract.controller.ts
+++ src/controllers/contract.controller.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
 import { ContractService } from '@services/contract.service';
+import { HttpException } from '@/exceptions/HttpException';
 
 export class ContractController {
   public contract = Container.get(ContractService);
 
   public getContractById = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const profileId = req.get('profile_id');
       const { id } = req.params;
-      const contract = await this.contract.findContractById(id as any);
-      if (!contract) return res.status(404).end();
+      const contract = await this.contract.findContractById(id as any, profileId as any);
+      if (!contract) throw new HttpException(404, "Contract doesn't exist");
       res.status(200).json(contract);
     } catch (error) {
       next(error);
